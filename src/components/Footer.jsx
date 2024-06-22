fe29cd3bd6347098015c086c57fa0016b94588b5
Dewansh Upadhyay <89184066+dew-op@users.noreply.github.com>
2024-06-22 11:14:07 +0530

    Tailwind Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='w-full px-20 py-10 bg-black text-white'>
             Footer
         </div>
     );
