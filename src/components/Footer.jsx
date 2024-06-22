025a7eeb9cd8195883d52f66cea727004b35af28
Dewansh Upadhyay <89184066+dew-op@users.noreply.github.com>
2024-06-22 13:22:38 +0530

    Add Weather Api to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,49 +4,52 @@ import { LuGlobe2 } from "react-icons/lu";
 import axios from 'axios';
 
 const Footer = () => {
-    // const [location, setLocation] = useState("Roorkee, India");
-    // const [dateTime, setDateTime] = useState("");
-    // const [temperature, setTemperature] = useState("");
-    // const [aqi, setAqi] = useState("");
+    const [location, setLocation] = useState("Roorkee, India");
+    const [dateTime, setDateTime] = useState("");
+    const [temperature, setTemperature] = useState("");
+    const [aqi, setAqi] = useState("");
 
-    // useEffect(() => {
-    //     const fetchWeatherData = async () => {
-    //       try {
-    //         const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
-    //         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Roorkee,in&units=metric&appid=${apiKey}`);
-            
-    //         const { temp } = response.data.main;
-    //         setTemperature(`Temp: ${temp}°C`);
-    
-    //         const coord = response.data.coord;
-    //         const aqiResponse = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}`);
-            
-    //         const { aqi } = aqiResponse.data.list[0].main;
-    //         setAqi(`AQI: ${aqi}`);
-    
-    //         // Set local datetime
-    //         const localDateTime = new Date().toLocaleString('en-IN', {
-    //           timeZone: response.data.timezone,
-    //           weekday: 'long',
-    //           hour: '2-digit',
-    //           minute: '2-digit',
-    //           day: 'numeric',
-    //           month: 'long',
-    //           year: 'numeric'
-    //         });
-    //         setDateTime(localDateTime);
-    //       } catch (error) {
-    //         console.error("Error fetching weather data:", error);
-    //       }
-    //     };
-    
-    //     fetchWeatherData();
-    // }, []);
-    
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            try {
+                const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Roorkee,in&units=metric&appid=${apiKey}`);
+                
+                const { temp } = response.data.main;
+                setTemperature(`Temp: ${temp}°C`);
+
+                const { lon, lat } = response.data.coord;
+                const aqiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+                
+                const { aqi } = aqiResponse.data.list[0].main;
+                setAqi(`AQI: ${aqi}`);
+
+                const timezoneOffset = response.data.timezone; // Timezone offset in seconds
+                const currentDate = new Date();
+                const utcDate = new Date(currentDate.getTime() + currentDate.getTimezoneOffset() * 60000);
+                const localDate = new Date(utcDate.getTime() + timezoneOffset * 1000);
+
+                const formattedDate = localDate.toLocaleString('en-IN', {
+                    weekday: 'long',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                });
+                
+                setDateTime(formattedDate);
+            } catch (error) {
+                console.error("Error fetching weather data:", error);
+            }
+        };
+
+        fetchWeatherData();
+    }, []);
 
     return (
         <div className='w-full flex py-16 bg-black text-white'>
-            <div className="flex flex-col w-[35%] items-center gap-8">
+            <div className="flex flex-col w-[35%] items-center gap-8 justify-center">
                 <img src="/images/iitr_logo.svg" className='w-24 h-24' alt="IITR" />
                 <div className="flex flex-col gap-1">
                     <h1 className="font-poppins text-lg">Indian Institute of Technology Roorkee</h1>
@@ -84,17 +87,16 @@ const Footer = () => {
             <div className="flex flex-col text-left px-20 gap-6 w-[40%]">
                 <div className="flex flex-col w-fit">
                     <h1 className="text-xl font-poppins">Roorkee</h1>
-                    <hr className="border-t-2 w-20 mb-4 border-white" />
-                    {/* <p className="text-gray-500 font-poppins">{dateTime}</p>
-                    <p className="text-gray-500 font-poppins">{temperature}</p>
-                    <p className="text-gray-500 font-poppins">{aqi}</p> */}
-                    <p className="text-gray-500 font-poppins">Monday 4:24 pm, 15th April, 2024<br />Temp : 33.09°C<br />AQI : 150</p>
+                    <hr className="border-t-2 w-20 mb-3 border-white" />
+                    <p className="text-gray-500 mb-1 font-poppins">{dateTime}</p>
+                    <p className="text-gray-500 mb-1 font-poppins">{temperature}</p>
+                    <p className="text-gray-500 mb-1 font-poppins">{aqi}</p>
                 </div>
                 <div className="flex flex-col w-full">
                     <h1 className="text-xl font-poppins">Find us here</h1>
                     <hr className="border-t-2 w-20 border-white" />
-                    <div className="mt-6">
-                        <iframe className='border-0 h-60 w-4/5 rounded-lg' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.9183662112623!2d77.89724787554691!3d29.866628025013625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb32bac1adc93%3A0x80bba7dd5e4d1a6a!2sStudents%20Activity%20Centre%20-%20SAC!5e0!3m2!1sen!2sin!4v1719039496909!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div className="mt-4">
+                        <iframe className='border-0 h-60 w-4/5 rounded-lg' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.9183662112623!2d77.89724787554691!3d29.866628025013625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb32bac1adc93%3A0x80bba7dd5e4d1a6a!2sStudents%20Activity%20Centre%20-%20SAC!5e0!3m2!1sen!2sin!4v1719039496909!5m2!1sen!2sin" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
